5c185f13c63bf6edb6f0beb0dee7683fa393d39a
YRafalsky <yrafalsky@spscommerce.com>
2014-09-25 21:53:21 +0300

    test

--- qqq.py
+++ qqq.py
@@ -7,6 +7,7 @@ results = dict()
 for s in zz:
     results[s.split("\t")[1]]=s.split("\t")[0]
 
+
 aa = git.Git().execute("git rev-parse deploy_to_server")
 aaa = git.Git().clone("https://github.com/HitroSoft/super-sea-battle-online.git","zzz")
 git.Git()
