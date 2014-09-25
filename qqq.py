2908c7ceec50f76601255fb1e4ef66a309beb3f5
YRafalsky <yrafalsky@spscommerce.com>
2014-09-26 01:14:46 +0300

    nice

--- qqq.py
+++ qqq.py
@@ -1,17 +1,61 @@
 __author__ = 'yrafalsky'
 import git
+import os
+import time
+import subprocess
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+target_folder= os.path.join(BASE_DIR,"super-sea-battle-online")
+if not os.path.exists(target_folder):
+    os.mkdir(target_folder)
+    git.Git().clone("https://github.com/HitroSoft/super-sea-battle-online.git",target_folder)
+os.chdir(target_folder)
+print os.getcwd()
+print "git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git checkout -b deploy_to_server origin/deploy_to_server"
+# zz=subprocess.call(["git","--work-tree="+target_folder,"--git-dir="+target_folder+"/.git","checkout","-b","deploy_to_server","origin/deploy_to_server"])
+# print zz
+aaz= subprocess.Popen(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git checkout -b deploy_to_server origin/deploy_to_server").split(" "),stdout=subprocess.PIPE)
+output, err = aaz.communicate()
+print output
+
+#aa = git.Git().execute("/usr/bin/git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git checkout -b deploy_to_server origin/deploy_to_server",with_raw_output=True,with_extended_output=True)
+    # git checkout -b test origin/test
+# if
 # git log -n 1 origin/creating_retranslator
-dd = git.Git().execute("git ls-remote git://github.com/HitroSoft/super-sea-battle-online")
-zz = dd.split("\n")
-results = dict()
-for s in zz:
-    results[s.split("\t")[1]]=s.split("\t")[0]
+subprocess.Popen("python manage.py runserver 0.0.0.0:8000".split(" "),stdout=subprocess.PIPE)
+print "server started"
+while True:
+    # dd = git.Git().execute("git ls-remote git://github.com/HitroSoft/super-sea-battle-online")
+    dd = subprocess.Popen("git ls-remote git://github.com/HitroSoft/super-sea-battle-online".split(" "),stdout=subprocess.PIPE)
+    output, err = dd.communicate()
+    print output
+    zz = output.split("\n")
+    results = dict()
+    for s in zz:
+        if s:
+            results[s.split("\t")[1]]=s.split("\t")[0]
+    print results['refs/heads/deploy_to_server']
+    # aa = git.Git().execute("git  --work-tree="+target_folder+" --git-dir="+target_folder+"/.git rev-parse deploy_to_server")
+    aa = subprocess.Popen(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git rev-parse deploy_to_server").split(" "),stdout=subprocess.PIPE)
+    output, err = aa.communicate()
+    print output
+    if output.split('\n')[0]!=results['refs/heads/deploy_to_server']:
+        print "changes detected"
+        aa=subprocess.Popen(['pgrep','-f',"/usr/bin/python manage.py"])
+        output, err = aa.communicate()
+        print output
+        subprocess.Popen(['sudo','kill','-9',output])
+        print "server killed"
+        subprocess.Popen(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git checkout -b deploy_to_server origin/deploy_to_server").split(" "),stdout=subprocess.PIPE)
+        subprocess.Popen("python manage.py runserver 0.0.0.0:8000".split(" "),stdout=subprocess.PIPE)
 
+        print "server started"
+    time.sleep(30)
 
-aa = git.Git().execute("git rev-parse deploy_to_server")
-aaa = git.Git().clone("https://github.com/HitroSoft/super-sea-battle-online.git","zzz")
-git.Git()
-
-repo = git.Repo("https://github.com/HitroSoft/super-sea-battle-online.git")
-print repo
-
+#
+#
+# os.chdir("super-sea-battle-online")
+# aa = git.Git().execute("git rev-parse deploy_to_server")
+# print aa
+# aaa = git.Git().clone("https://github.com/HitroSoft/super-sea-battle-online.git","zzz")
+#
