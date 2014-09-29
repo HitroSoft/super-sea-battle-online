d2e9a8adfdff127773aa4f04f072fd3354ebc03a
YRafalsky <yrafalsky@spscommerce.com>
2014-09-29 19:09:08 +0300

    fix

--- various_data/create_environment.py
+++ various_data/create_environment.py
@@ -21,9 +21,33 @@ import os
 import time
 import subprocess
 
+def exec_command_and_get_result(the_command):
+    print("=================")
+    aaz= subprocess.Popen((the_command).split(" "),stdout=subprocess.PIPE)
+    output, err = aaz.communicate()
+    print ("Executed=" + the_command)
+    print ("Result=" + output)
+    print("Errors = " + str(err))
+    return output
+
+def fetch_from_remote_github(pre_checkout=True):
+    if pre_checkout:
+        print exec_command_and_get_result("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git checkout --force -b deploy_to_server origin/deploy_to_server")
+    exec_command_and_get_result("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git fetch origin")
+    exec_command_and_get_result("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git reset --hard origin/deploy_to_server")
+
+def kill_server():
+    bb=subprocess.Popen(['pgrep','-f',"/usr/bin/python manage.py"],stdout=subprocess.PIPE)
+    output, err = bb.communicate()
+    print "grep_result = " + output
+    print "process_id="+output.split("\n")[0]
+    subprocess.Popen(['sudo','kill','-9',output.split("\n")[0]])
+    print "server killed"
+
+
 # prepare everything for this script work
-subprocess.call(("apt-get install git -y").split(" "))
-subprocess.call(("pip install GitPython").split(" "))
+exec_command_and_get_result("apt-get install git -y")
+exec_command_and_get_result("pip install GitPython")
 
 import git
 
@@ -34,40 +58,24 @@ if not os.path.exists(target_folder):
     git.Git().clone("https://github.com/HitroSoft/super-sea-battle-online.git",target_folder)
 os.chdir(target_folder)
 
-aaz= subprocess.Popen(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git checkout --force -b deploy_to_server origin/deploy_to_server").split(" "),stdout=subprocess.PIPE)
-output, err = aaz.communicate()
-print output
-subprocess.call(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git fetch origin").split(" "))
-subprocess.call(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git reset --hard origin/deploy_to_server").split(" "))
+fetch_from_remote_github(pre_checkout=True)
 
-
-subprocess.Popen("python manage.py runserver 0.0.0.0:8000".split(" "),stdout=subprocess.PIPE)
+exec_command_and_get_result("python manage.py runserver 0.0.0.0:8000")
 print "server started"
 while True:
 
-    dd = subprocess.Popen("git ls-remote git://github.com/HitroSoft/super-sea-battle-online".split(" "),stdout=subprocess.PIPE)
-    output, err = dd.communicate()
-    print output
+    output = exec_command_and_get_result("git ls-remote git://github.com/HitroSoft/super-sea-battle-online")
     zz = output.split("\n")
     results = dict()
     for s in zz:
         if s:
             results[s.split("\t")[1]]=s.split("\t")[0]
     print results['refs/heads/deploy_to_server']
-
-    aa = subprocess.Popen(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git rev-parse deploy_to_server").split(" "),stdout=subprocess.PIPE)
-    output, err = aa.communicate()
-    print output
+    output = exec_command_and_get_result("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git rev-parse deploy_to_server")
     if output.split('\n')[0]!=results['refs/heads/deploy_to_server']:
         print "changes detected"
-        bb=subprocess.Popen(['pgrep','-f',"/usr/bin/python manage.py"],stdout=subprocess.PIPE)
-        output, err = bb.communicate()
-        print "process_id="+output.split("\n")[0]
-        subprocess.Popen(['sudo','kill','-9',output.split("\n")[0]])
-        print "server killed"
-        subprocess.call(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git fetch origin").split(" "))
-        subprocess.call(("git --work-tree="+target_folder+" --git-dir="+target_folder+"/.git reset --hard origin/deploy_to_server").split(" "))
-        subprocess.Popen("python manage.py runserver 0.0.0.0:8000".split(" "),stdout=subprocess.PIPE)
-
+        kill_server()
+        fetch_from_remote_github(pre_checkout=False)
+        exec_command_and_get_result("python manage.py runserver 0.0.0.0:8000")
         print "server started"
     time.sleep(30)
