7b2987b216521b0b0a235f4aca05e72e7fe37c0b
YRafalsky <yrafalsky@spscommerce.com>
2014-09-30 11:08:16 +0300

    code formatting

--- superseabattleonline/api/views.py
+++ superseabattleonline/api/views.py
@@ -16,12 +16,13 @@ import os
 from ai_palyer import AIplayer
 from common_data import ships_states, game_list
 
+
 def get_value_from_config(name):
     try:
         config = ConfigParser.RawConfigParser()
         filename = os.path.join(BASE_DIR, "data/bot_dictionary.conf")
         config.read(filename)
-        return config.get(name,"message")
+        return config.get(name, "message")
     except Exception as e:
         print (e)
         return None
@@ -31,7 +32,6 @@ lock = threading.Lock()
 
 
 class Game(object):
-
     def __init__(self):
         self.game_name = ''.join(random.choice(string.ascii_letters) for _ in range(12))
         self.battlefield_first_player = None
@@ -58,7 +58,8 @@ class Game(object):
                 cell['STATE'] = ships_states['INITIALIZED']
         self.first_player_to_message_queue = []
         self.first_player_response_id += 1
-        self.first_player_to_message_queue.append({"name":"waiting-for-rival","id":self.first_player_response_id,"start":int(time.time())})
+        self.first_player_to_message_queue.append(
+            {"name": "waiting-for-rival", "id": self.first_player_response_id, "start": int(time.time())})
 
         self.game_state = "waiting-for-rival"
         return
@@ -75,13 +76,17 @@ class Game(object):
         self.second_player_response_id = 0
         self.second_player_response_received_id = 0
         self.first_player_response_id += 1
-        self.first_player_to_message_queue.append({"name":"game-started-move-on","id":self.first_player_response_id,"start":int(time.time())})
+        self.first_player_to_message_queue.append(
+            {"name": "game-started-move-on", "id": self.first_player_response_id, "start": int(time.time())})
         self.second_player_response_id += 1
-        self.second_player_to_message_queue.append({"name":"game-started-move-off","id":self.second_player_response_id,"start":int(time.time())})
+        self.second_player_to_message_queue.append(
+            {"name": "game-started-move-off", "id": self.second_player_response_id, "start": int(time.time())})
         self.game_state = "game-started-first-players-move"
 
         self.first_player_response_id += 1
-        self.first_player_to_message_queue.append({"name":"chat-message","id":self.first_player_response_id,"start":int(time.time()),"payload":{"message":get_value_from_config("Greetings"),"owner":"qwe","date":int(time.time())}})
+        self.first_player_to_message_queue.append(
+            {"name": "chat-message", "id": self.first_player_response_id, "start": int(time.time()),
+             "payload": {"message": get_value_from_config("Greetings"), "owner": "qwe", "date": int(time.time())}})
         return
 
     def start_game(self, data):
@@ -96,7 +101,8 @@ class Game(object):
         self.first_player_to_message_queue = []
 
         self.first_player_response_id += 1
-        self.first_player_to_message_queue.append({"name":"waiting-for-rival","id":self.first_player_response_id,"start":int(time.time())})
+        self.first_player_to_message_queue.append(
+            {"name": "waiting-for-rival", "id": self.first_player_response_id, "start": int(time.time())})
 
         return
 
@@ -111,23 +117,23 @@ class Game(object):
         first_player_ships_killed = True
         for ship in self.battlefield_first_player:
             for cell in ship:
-                if cell['STATE']not in [ships_states['KILLED'],ships_states['WOUNDED']]:
+                if cell['STATE'] not in [ships_states['KILLED'], ships_states['WOUNDED']]:
                     first_player_ships_killed = False
                     break
         second_player_ships_killed = True
         for ship in self.battlefield_second_player:
             for cell in ship:
-                if cell['STATE']not in [ships_states['KILLED'],ships_states['WOUNDED']]:
+                if cell['STATE'] not in [ships_states['KILLED'], ships_states['WOUNDED']]:
                     second_player_ships_killed = False
                     break
-        if first_player_ships_killed==True:
+        if first_player_ships_killed == True:
             return 2
-        if second_player_ships_killed==True:
+        if second_player_ships_killed == True:
             return 1
         return 0
 
-    def make_player_shoot(self,user_num,x,y):
-        if user_num==1:
+    def make_player_shoot(self, user_num, x, y):
+        if user_num == 1:
             target_battlefield = self.battlefield_second_player
         else:
             target_battlefield = self.battlefield_first_player
@@ -135,12 +141,12 @@ class Game(object):
 
         for ship in target_battlefield:
             for cell in ship:
-                if x == cell['x'] and y ==cell['y']:
-                    cell['STATE']=ships_states['WOUNDED']
-                    ship_killed=True
+                if x == cell['x'] and y == cell['y']:
+                    cell['STATE'] = ships_states['WOUNDED']
+                    ship_killed = True
                     for cell2 in ship:
                         if cell2['STATE'] != ships_states['WOUNDED'] and cell2['STATE'] != ships_states['KILLED']:
-                            ship_killed=False
+                            ship_killed = False
                     if ship_killed:
                         for cell2 in ship:
                             cell2['STATE'] = ships_states['KILLED']
@@ -148,18 +154,18 @@ class Game(object):
                     else:
                         result = ships_states['WOUNDED']
         killer = self.check_are_all_ships_killed(1)
-        if killer!=0:
-            if killer==1:
+        if killer != 0:
+            if killer == 1:
                 first_user_action = 'game-over-win'
                 second_user_action = 'game-over-lose'
                 self.game_state = "first-player-win"
-            elif killer==2:
+            elif killer == 2:
                 second_user_action = 'game-over-win'
                 first_user_action = 'game-over-lose'
                 self.game_state = "second-player-win"
         else:
             if result in [ships_states['WOUNDED'], ships_states['KILLED']]:
-                if user_num==1:
+                if user_num == 1:
                     self.game_state = "first-player-move"
                     first_user_action = "move-on"
                     second_user_action = "move-off"
@@ -168,7 +174,7 @@ class Game(object):
                     first_user_action = "move-off"
                     second_user_action = "move-on"
             else:
-                if user_num==1:
+                if user_num == 1:
                     self.game_state = "second-player-move"
                     first_user_action = "move-off"
                     second_user_action = "move-on"
@@ -176,21 +182,28 @@ class Game(object):
                     self.game_state = "first-player-move"
                     first_user_action = "move-on"
                     second_user_action = "move-off"
-        if user_num==1:
+        if user_num == 1:
             self.first_player_response_id += 1
-            self.first_player_to_message_queue.append({"name":first_user_action,"id":self.first_player_response_id,"start":int(time.time()), "payload":{"register-self-shoot":{"y":y,"x":x,"state":result}}})
+            self.first_player_to_message_queue.append(
+                {"name": first_user_action, "id": self.first_player_response_id, "start": int(time.time()),
+                 "payload": {"register-self-shoot": {"y": y, "x": x, "state": result}}})
             self.second_player_response_id += 1
-            self.second_player_to_message_queue.append({"name":second_user_action,"id":self.second_player_response_id,"start":int(time.time()), "payload":{"register-rival-shoot":{"y":y,"x":x,"state":result}}})
+            self.second_player_to_message_queue.append(
+                {"name": second_user_action, "id": self.second_player_response_id, "start": int(time.time()),
+                 "payload": {"register-rival-shoot": {"y": y, "x": x, "state": result}}})
         else:
             self.first_player_response_id += 1
-            self.first_player_to_message_queue.append({"name":first_user_action,"id":self.first_player_response_id,"start":int(time.time()), "payload":{"register-rival-shoot":{"y":y,"x":x,"state":result}}})
+            self.first_player_to_message_queue.append(
+                {"name": first_user_action, "id": self.first_player_response_id, "start": int(time.time()),
+                 "payload": {"register-rival-shoot": {"y": y, "x": x, "state": result}}})
             self.second_player_response_id += 1
-            self.second_player_to_message_queue.append({"name":second_user_action,"id":self.second_player_response_id,"start":int(time.time()), "payload":{"register-self-shoot":{"y":y,"x":x,"state":result}}})
+            self.second_player_to_message_queue.append(
+                {"name": second_user_action, "id": self.second_player_response_id, "start": int(time.time()),
+                 "payload": {"register-self-shoot": {"y": y, "x": x, "state": result}}})
         return
 
-    def leave_game(self,user_id): # todo make this functionality
+    def leave_game(self, user_id):  # todo make this functionality
         self.is_game_active = False
-
 
 
 def find_game_by_name(user_id):
@@ -199,6 +212,7 @@ def find_game_by_name(user_id):
             return game
     return None
 
+
 def create_game(user_id, json_data):
     game = Game()
     game.add_first_player(json_data)
@@ -206,50 +220,54 @@ def create_game(user_id, json_data):
     user_id = game.first_player_id
     events = game.get_unreceived_events_for_first_player()
     lock.release()
-    return HttpResponse(content=json.dumps({"id":user_id, "events":events}),
-                content_type='application/json',
-                status=200)
+    return HttpResponse(content=json.dumps({"id": user_id, "events": events}),
+                        content_type='application/json',
+                        status=200)
+
 
 def waiting_for_event(user_id, json_data):
     game = find_game_by_name(user_id)
     if game is None:
         raise Exception("Game not found - no create event was detected")
-    if game.game_state=="waiting-for-rival" and game.ai_allowed:
+    if game.game_state == "waiting-for-rival" and game.ai_allowed:
         game.attach_ai_player()
     if 'reid' in json_data:
         game.first_player_response_received_id = json_data['reid']
     events = game.get_unreceived_events_for_first_player()
-    if events.__len__()==0 and game.game_state=="second-player-move" and random.randint(0,3) == 0 and game.ai_allowed:
-        x,y = game.ai_player.make_shoot()
-        game.make_player_shoot(user_num=2,x=x, y=y)
+    if events.__len__() == 0 and game.game_state == "second-player-move" and random.randint(0,
+                                                                                            3) == 0 and game.ai_allowed:
+        x, y = game.ai_player.make_shoot()
+        game.make_player_shoot(user_num=2, x=x, y=y)
         events = game.get_unreceived_events_for_first_player()
     lock.release()
-    if events.__len__()==0:
+    if events.__len__() == 0:
         time.sleep(0.5)
-        return HttpResponse(content=json.dumps({"id":user_id, "events":events}),
-                content_type='application/json',
-                status=504)
+        return HttpResponse(content=json.dumps({"id": user_id, "events": events}),
+                            content_type='application/json',
+                            status=504)
     else:
-        return HttpResponse(content=json.dumps({"id":user_id, "events":events}),
-                content_type='application/json',
-                status=200)
+        return HttpResponse(content=json.dumps({"id": user_id, "events": events}),
+                            content_type='application/json',
+                            status=200)
+
+
 def register_shoot(user_id, json_data):
     game = find_game_by_name(user_id)
     if game is None:
         raise Exception("Game not found - no create event was detected")
-    if game.game_state != "game-started-first-players-move" and  game.game_state != "first-player-move":
+    if game.game_state != "game-started-first-players-move" and game.game_state != "first-player-move":
         print "Error in game - game turn error!"
         return HttpResponse(content="Error in game",
-                content_type='application/json',
-                status=501)
+                            content_type='application/json',
+                            status=501)
     x = json_data['shoot']['x']
     y = json_data['shoot']['y']
-    game.make_player_shoot(user_num=1,x=x, y=y)
+    game.make_player_shoot(user_num=1, x=x, y=y)
     events = game.get_unreceived_events_for_first_player()
     lock.release()
-    return HttpResponse(content=json.dumps({"id":user_id, "events":events}),
-                content_type='application/json',
-                status=200)
+    return HttpResponse(content=json.dumps({"id": user_id, "events": events}),
+                        content_type='application/json',
+                        status=200)
 
 
 class Imitator(APIView):
@@ -263,28 +281,28 @@ class Imitator(APIView):
                 user_id = kw['pk']
                 zz = request.body
                 json_data = json.loads(request.body)
-                if json_data['command']=="leave":
+                if json_data['command'] == "leave":
                     return create_game(user_id=user_id, json_data=json_data)
-                if json_data['command']=="create":
+                if json_data['command'] == "create":
                     return create_game(user_id=user_id, json_data=json_data)
-                if json_data['command']=="waiting-for-event":
-                    return waiting_for_event(user_id=user_id,json_data=json_data)
-                if json_data['command']=="register-shoot":
+                if json_data['command'] == "waiting-for-event":
+                    return waiting_for_event(user_id=user_id, json_data=json_data)
+                if json_data['command'] == "register-shoot":
                     return register_shoot(user_id=user_id, json_data=json_data)
 
                 lock.release()
                 print "Unknown command " + str(json_data)
                 return HttpResponse(content=None,
-                                content_type='application/json',
-                                status=504)
+                                    content_type='application/json',
+                                    status=504)
 
             except requests.Timeout as timeout:
                 print("Request failed by timeout")
                 print("Request took " + str(time.time() - start_time) + " seconds")
                 lock.release()
                 return HttpResponse(content=None,
-                                content_type='application/json',
-                                status=504)
+                                    content_type='application/json',
+                                    status=504)
             except Exception as e:
                 print("Request failed: " + str(e))
                 print("Request took " + str(time.time() - start_time) + " seconds")
